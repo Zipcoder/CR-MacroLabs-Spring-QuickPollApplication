1aeda3d700a25cc727f7fc3931b2698a751520c3
Lawrence Wu <yl981@scarletmail.rutgers.edu>
2018-04-03 19:07:58 -0400

    up to part 2.3

---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/repositories/OptionRepository.java
@@ -0,0 +1,7 @@
+package io.zipcoder.tc_spring_poll_application.repositories;
+
+import io.zipcoder.tc_spring_poll_application.domain.Poll;
+import org.springframework.data.repository.CrudRepository;
+
+interface OptionRepository extends CrudRepository<Poll, Long>{
+}
