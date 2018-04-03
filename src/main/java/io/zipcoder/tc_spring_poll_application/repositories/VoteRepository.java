76e7b37d233886ad06e7154dec048f8e2b352a6d
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-04-03 16:09:54 -0400

    Repository interfaces

---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/repositories/VoteRepository.java
@@ -0,0 +1,10 @@
+package io.zipcoder.tc_spring_poll_application.repositories;
+
+import io.zipcoder.tc_spring_poll_application.domain.Vote;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VoteRepository extends CrudRepository<Vote, Long>{
+
+
+
+}
