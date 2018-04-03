e0e3d4453abb00520c408ded160c3e8611361560
PeterMcCormick <mcpeter@udel.edu>
2018-04-03 15:58:14 -0400

    up to last step of Part1.1

---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/domain/Option.java
@@ -0,0 +1,21 @@
+package io.zipcoder.tc_spring_poll_application.domain;
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
+public class Option {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "OPTION_ID")
+    private long id;
+
+    @Column(name = "OPTION_VALUE")
+
+}
