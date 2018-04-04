5894f260c81c97259c039be25f016e97e07adf2c
Patrick Glavin <patjglavin@gmail.com>
2018-04-04 17:33:52 -0400

    add voterepo interface

--- src/main/java/io/zipcoder/tc_spring_poll_application/repositories/OptionRepository.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/repositories/OptionRepository.java
@@ -1,4 +1,8 @@
 package io.zipcoder.tc_spring_poll_application.repositories;
 
-public interface OptionRepository {
+import io.zipcoder.tc_spring_poll_application.domain.Option;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OptionRepository extends CrudRepository<Option, Long>{
+
 }
--- src/main/java/io/zipcoder/tc_spring_poll_application/repositories/PollRepository.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/repositories/PollRepository.java
@@ -1,4 +1,7 @@
 package io.zipcoder.tc_spring_poll_application.repositories;
 
-public interface PollRepository {
+import io.zipcoder.tc_spring_poll_application.domain.Poll;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PollRepository extends CrudRepository<Poll, Long>{
 }
