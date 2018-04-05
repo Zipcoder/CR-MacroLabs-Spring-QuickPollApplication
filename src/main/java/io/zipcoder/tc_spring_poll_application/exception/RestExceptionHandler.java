98c930c3e977653bea0be868e3a152f289cb3018
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-04-05 16:59:28 -0400

    Rest exception handler

---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/exception/RestExceptionHandler.java
@@ -0,0 +1,23 @@
+package io.zipcoder.tc_spring_poll_application.exception;
+
+import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
+import dtos.error.ErrorDetail;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException rnfe, HttpServletRequest request) {
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setTimeStamp(new Date().getTime());
+        errorDetail.setStatus(HttpStatus.NOT_FOUND.value());
+        errorDetail.setTitle("Resource Not Found");
+        errorDetail.setDetail(rnfe.getMessage());
+        errorDetail.setDeveloperMessage(rnfe.getClass().getName());
+        return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
+    }
+}
