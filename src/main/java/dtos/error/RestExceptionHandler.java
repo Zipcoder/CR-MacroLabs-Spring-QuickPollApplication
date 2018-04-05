2488c27428a4f30ffef3dd4e5b3bbdad77ef677e
Anthony Jordan <Kain235@gmail.com>
2018-04-05 16:53:15 -0400

    Finished part 5.7

--- src/main/java/dtos/error/RestExceptionHandler.java
+++ src/main/java/dtos/error/RestExceptionHandler.java
@@ -1,6 +1,8 @@
 package dtos.error;
 
 import io.zipcoder.tc_spring_poll_application.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -15,6 +17,10 @@ import java.util.List;
 
 @ControllerAdvice
 public class RestExceptionHandler {
+
+    @Autowired()
+    private MessageSource messageSource;
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException rnfe, HttpServletRequest request) {
         ErrorDetail errorDetail = new ErrorDetail();
@@ -41,7 +47,7 @@ public class RestExceptionHandler {
             }
             ValidationError validationError = new ValidationError();
             validationError.setCode(fe.getCode());
-            validationError.setMessage(fe.getDefaultMessage());
+            validationError.setMessage(messageSource.getMessage(fe,null));
             validationErrorList.add(validationError);
         }
         errorDetail.setTimeStamp(new Date().getTime());
