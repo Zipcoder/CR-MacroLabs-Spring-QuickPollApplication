5516cbb103ccdad3b637cf4abc74d1eecf420fc9
PeterMcCormick <mcpeter@udel.edu>
2018-04-04 22:22:15 -0400

    Up to start of 3.2

--- src/main/java/io/zipcoder/tc_spring_poll_application/domain/Option.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/domain/Option.java
@@ -17,5 +17,21 @@ public class Option {
     private long id;
 
     @Column(name = "OPTION_VALUE")
+    private String value;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
