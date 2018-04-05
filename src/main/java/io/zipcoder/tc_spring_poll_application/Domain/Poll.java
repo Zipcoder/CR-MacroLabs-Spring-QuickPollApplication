b311ea714830e36acee1f95c8ad16a70756365f8
Kthomas <kthomas@github.com>
2018-04-05 17:18:45 -0400

    Part 5.2

--- src/main/java/io/zipcoder/tc_spring_poll_application/Domain/Poll.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/Domain/Poll.java
@@ -1,4 +1,51 @@
 package io.zipcoder.tc_spring_poll_application.Domain;
 
+import org.hibernate.validator.constraints.NotEmpty;
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.Set;
+/**
+ * @Another Pojojojo
+ */
+
+@Entity
 public class Poll {
+
+    @Id
+    @GeneratedValue
+    @Column (name = "POLL_ID")
+    private long id;
+
+    @Column (name = "QUESTION")
+    @NotEmpty
+    private String question;
+
+    @OneToMany (cascade = CascadeType.ALL)
+    @JoinColumn (name = "POLL_ID")
+    @OrderBy
+    private Set<Option> options;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public Set getOptions() {
+        return options;
+    }
+
+    public void setOptions(Set<Option> options) {
+        this.options = options;
+    }
 }
