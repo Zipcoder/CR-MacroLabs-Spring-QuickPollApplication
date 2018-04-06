09c6ba37acd5fec6f0d57fd4bbd39f8cefb76b6d
Carolynn Vansant <cvansant@MacBook-Pro-7.local>
2018-04-06 10:14:44 -0400

    up through 4.3

---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controller/ComputeResultController.java
@@ -0,0 +1,28 @@
+package io.zipcoder.tc_spring_poll_application.controller;
+
+import dtos.VoteResult;
+import io.zipcoder.tc_spring_poll_application.domain.Vote;
+import io.zipcoder.tc_spring_poll_application.repositories.VoteRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
+
+@RestController
+public class ComputeResultController {
+    @Inject
+    private VoteRepository voteRepository;
+
+    @RequestMapping(value = "/computeresult", method = RequestMethod.GET)
+    public ResponseEntity<?> computeResult(@RequestParam Long pollId) {
+        VoteResult voteResult = new VoteResult();
+        Iterable<Vote> allVotes = voteRepository.findByPoll(pollId);
+
+        //TODO: Implement algorithm to count votes
+        return new ResponseEntity<VoteResult>(voteResult, HttpStatus.OK);
+    }
+}
