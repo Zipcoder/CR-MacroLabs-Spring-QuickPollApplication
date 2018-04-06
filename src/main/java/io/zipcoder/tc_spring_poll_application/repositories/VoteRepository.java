f0d3174e5bcb7ec58ac82f2ab96a02d024bdaa89
CWinarski <Winarski70@yahoo.com>
2018-04-05 23:31:18 -0400

    Saving for the night

--- src/main/java/io/zipcoder/tc_spring_poll_application/repositories/VoteRepository.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/repositories/VoteRepository.java
@@ -1,9 +1,16 @@
 package io.zipcoder.tc_spring_poll_application.repositories;
 
 import io.zipcoder.tc_spring_poll_application.domain.Vote;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface VoteRepository extends CrudRepository<Vote, Long> {
     //these are DAOs pr Data Access Objects they provide abstraction for interacting with data stores
     // ypu have usually one repository per domain object
+
+    @Query(value = "SELECT v.* " +
+            "FROM Option o, Vote v " +
+            "WHERE o.POLL_ID = ?1 " +
+            "AND v.OPTION_ID = o.OPTION_ID", nativeQuery = true)
+    public Iterable<Vote> findVotesByPoll(Long pollId);
 }
