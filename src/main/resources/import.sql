6d9891037c93c65a819ebf701adb251413e88ee3
Lawrence Wu <yl981@scarletmail.rutgers.edu>
2018-04-06 11:10:06 -0400

    added more polls

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,5 +3,17 @@ insert into option (option_id, option_value, poll_id) values (1, 'Red', 1);
 insert into option (option_id, option_value, poll_id) values (2, 'Blue', 1);
 insert into option (option_id, option_value, poll_id) values (3, 'Yellow', 1);
 
+insert into poll (poll_id, question) values (2, 'What is your favorite movie?');
+insert into option (option_id, option_value, poll_id) values (4, 'Mad Max: Fury Road', 2);
+insert into option (option_id, option_value, poll_id) values (5, 'Blade Runner 2049', 2);
+insert into option (option_id, option_value, poll_id) values (6, 'The Dark Knight', 2);
 
+insert into poll (poll_id, question) values (3, 'What is your favorite time?');
+insert into option (option_id, option_value, poll_id) values (7, 'Three O Clock', 3);
+insert into option (option_id, option_value, poll_id) values (8, 'Five O Clock', 3);
+insert into option (option_id, option_value, poll_id) values (9, 'One O Clock', 3);
 
+insert into poll (poll_id, question) values (4, 'What is your best life?');
+insert into option (option_id, option_value, poll_id) values (10, 'Living it', 4);
+insert into option (option_id, option_value, poll_id) values (11, 'Not Living it', 4);
+insert into option (option_id, option_value, poll_id) values (12, 'Nah', 4);
