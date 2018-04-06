4032df83dcede3cbb64744e82b340e2e558a4763
Carolynn Vansant <cvansant@MacBook-Pro-7.fios-router.home>
2018-04-05 21:20:04 -0400

    made progress

--- src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
@@ -4,7 +4,7 @@ import io.zipcoder.tc_spring_poll_application.repositories.PollRepository;
 import io.zipcoder.tc_spring_poll_application.domain.Poll;
 
 
-
+import io.zipcoder.tc_spring_poll_application.repositories.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -18,8 +18,12 @@ import java.net.URI;
 @RestController
 public class PollController {
 
-    @Autowired
     private PollRepository pollRepository;
+
+    @Autowired
+    public PollController(PollRepository pollRepository) {
+        this.pollRepository = pollRepository;
+    }
 
     @RequestMapping(value="/polls", method= RequestMethod.GET)
     public ResponseEntity<Iterable<Poll>> getAllPolls() {
