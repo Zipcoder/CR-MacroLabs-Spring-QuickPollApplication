b3fa0ba3fc896475bae834a15e7237b352d5f7b4
Karousha Fennimore <karousha@gmail.com>
2018-04-06 23:28:51 -0400

    done

---
+++ src/main/resources/import.sql
@@ -0,0 +1,78 @@
+insert into poll (poll_id, question) values (1, 'What is your favorite color?');
+insert into option (option_id, option_value, poll_id) values (1, 'Red', 1);
+insert into option (option_id, option_value, poll_id) values (2, 'Blue', 1);
+insert into option (option_id, option_value, poll_id) values (3, 'Green', 1);
+insert into option (option_id, option_value, poll_id) values (4, 'Purple', 1);
+
+insert into poll (poll_id, question) values (1, 'What is your favorite food?');
+insert into option (option_id, option_value, poll_id) values (11, 'Italian', 2);
+insert into option (option_id, option_value, poll_id) values (12, 'Seafood', 2);
+insert into option (option_id, option_value, poll_id) values (13, 'Mexican', 2);
+
+insert into poll (poll_id, question) values (1, 'What is your favorite dance?');
+insert into option (option_id, option_value, poll_id) values (21, 'Tango', 3);
+insert into option (option_id, option_value, poll_id) values (22, 'Waltz', 3);
+insert into option (option_id, option_value, poll_id) values (23, 'Ballroom', 3);
+
+insert into poll (poll_id, question) values (1, 'What is your favorite drink?');
+insert into option (option_id, option_value, poll_id) values (31, 'Water', 4);
+insert into option (option_id, option_value, poll_id) values (32, 'Coke', 4);
+insert into option (option_id, option_value, poll_id) values (33, 'Sprite', 4);
+
+insert into poll (poll_id, question) values (1, 'Where is your favorite country?');
+insert into option (option_id, option_value, poll_id) values (41, 'USA', 5);
+insert into option (option_id, option_value, poll_id) values (42, 'France', 5);
+insert into option (option_id, option_value, poll_id) values (43, 'Sweden', 5);
+
+insert into poll (poll_id, question) values (1, 'What is your age group?');
+insert into option (option_id, option_value, poll_id) values (51, '18-25', 6);
+insert into option (option_id, option_value, poll_id) values (52, '26-30', 6);
+insert into option (option_id, option_value, poll_id) values (53, '31-35', 6);
+
+insert into poll (poll_id, question) values (1, 'Is the world flat?');
+insert into option (option_id, option_value, poll_id) values (61, 'Yes', 7);
+insert into option (option_id, option_value, poll_id) values (62, 'No', 7);
+insert into option (option_id, option_value, poll_id) values (63, 'Purple', 7);
+
+insert into poll (poll_id, question) values (1, 'Where do you live?');
+insert into option (option_id, option_value, poll_id) values (71, 'PA', 8);
+insert into option (option_id, option_value, poll_id) values (72, 'DE', 8);
+insert into option (option_id, option_value, poll_id) values (73, 'NJ', 8);
+
+insert into poll (poll_id, question) values (1, 'What is your preferred way of travel?');
+insert into option (option_id, option_value, poll_id) values (81, 'Bus', 9);
+insert into option (option_id, option_value, poll_id) values (82, 'Plane', 9);
+insert into option (option_id, option_value, poll_id) values (83, 'Car', 9);
+
+insert into poll (poll_id, question) values (1, 'Can you turn right on red?');
+insert into option (option_id, option_value, poll_id) values (91, 'Yes', 10);
+insert into option (option_id, option_value, poll_id) values (92, 'No', 10);
+insert into option (option_id, option_value, poll_id) values (93, 'It depends', 10);
+
+insert into poll (poll_id, question) values (1, 'What is the speed limit in a residential area?');
+insert into option (option_id, option_value, poll_id) values (101, '38', 11);
+insert into option (option_id, option_value, poll_id) values (102, '45', 11);
+insert into option (option_id, option_value, poll_id) values (103, '25', 11);
+
+insert into poll (poll_id, question) values (1, 'Which age did you start to drive?');
+insert into option (option_id, option_value, poll_id) values (111, '20+', 12);
+insert into option (option_id, option_value, poll_id) values (112, '16', 12);
+insert into option (option_id, option_value, poll_id) values (113, '18', 12);
+
+insert into poll (poll_id, question) values (1, 'What is your favorite movie?');
+insert into option (option_id, option_value, poll_id) values (121, 'Count of Monte Cristo', 13);
+insert into option (option_id, option_value, poll_id) values (122, 'The Fly', 13);
+insert into option (option_id, option_value, poll_id) values (123, 'The Eternal Sunshine of the Spotless Mind', 13);
+insert into option (option_id, option_value, poll_id) values (123, 'Ever After', 13);
+
+insert into poll (poll_id, question) values (1, 'Which musical genre do you prefer?');
+insert into option (option_id, option_value, poll_id) values (131, 'Jazz', 14);
+insert into option (option_id, option_value, poll_id) values (132, 'Hip-Hop', 14);
+insert into option (option_id, option_value, poll_id) values (133, 'Reggae', 14);
+insert into option (option_id, option_value, poll_id) values (133, 'Pop', 14);
+
+
+insert into poll (poll_id, question) values (1, 'What life should you live?');
+insert into option (option_id, option_value, poll_id) values (141, 'The one you have', 15);
+insert into option (option_id, option_value, poll_id) values (142, 'Your best one', 15);
+insert into option (option_id, option_value, poll_id) values (143, 'N/A', 15);
