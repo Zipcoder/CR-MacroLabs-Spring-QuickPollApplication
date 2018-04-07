30265dd80e8db6bb5cb53176b88e57bc1557d552
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-04-07 18:18:36 -0400

    woops

--- src/main/java/io/zipcoder/tc_spring_poll_application/controller/ComputeResultController.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controller/ComputeResultController.java
@@ -1,6 +1,7 @@
 package io.zipcoder.tc_spring_poll_application.controller;
 
 import io.zipcoder.tc_spring_poll_application.domain.Vote;
+import io.zipcoder.tc_spring_poll_application.dto.OptionCount;
 import io.zipcoder.tc_spring_poll_application.dto.VoteResult;
 import io.zipcoder.tc_spring_poll_application.repositories.VoteRepository;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class ComputeResultController {
@@ -20,7 +23,22 @@ public class ComputeResultController {
         VoteResult voteResult = new VoteResult();
         Iterable<Vote> allVotes = voteRepository.findVotesByPoll(pollId);
 
-        return new ResponseEntity<VoteResult>(voteResult, HttpStatus.OK);
+        int totalVotes = 0;
+        Map<Long, OptionCount> tempMap = new HashMap<>();
+        for(Vote v : allVotes) {
+            totalVotes++;
+            OptionCount optionCount = tempMap.get(v.getOption().getId());
+            if(optionCount == null) {
+                optionCount = new OptionCount();
+                optionCount.setOptionId(v.getOption().getId());
+                tempMap.put(v.getOption().getId(), optionCount);
+            }
+            optionCount.setCount(optionCount.getCount() + 1);
+        }
+        voteResult.setTotalVotes(totalVotes);
+        voteResult.setResults(tempMap.values());
+
+        return new ResponseEntity<>(voteResult, HttpStatus.OK);
     }
 
 }
