63b0ddee50fae4d0df1b498d74fb3c425a1d4867
Patrick Glavin <patjglavin@gmail.com>
2018-04-08 14:31:53 -0400

    finalize error messaging

--- src/main/java/io/zipcoder/tc_spring_poll_application/exception/ErrorDetail.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/exception/ErrorDetail.java
@@ -1,11 +1,23 @@
 package io.zipcoder.tc_spring_poll_application.exception;
 
+import java.util.List;
+import java.util.Map;
+
 public class ErrorDetail {
     private String title;
     private int status;
     private String detail;
     private long timeStamp;
     private String developerMessage;
+    private Map<String, List<ValidationError>> errors;
+
+    public Map<String, List<ValidationError>> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(Map<String, List<ValidationError>> errors) {
+        this.errors = errors;
+    }
 
     public String getTitle() {
         return title;
