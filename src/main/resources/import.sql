9cb7df6e43595272de8e94bf73f65c509a7fc922
Bo Lee <bolee@zipcodersmbp10.lan>
2018-04-07 23:32:32 -0400

    complete

---
+++ src/main/resources/import.sql
@@ -0,0 +1,90 @@
+insert into poll (poll_id, question) values (1, 'Which color do your prefer?');
+
+insert into option (option_id, option_value, poll_id) values (1, 'Red', 1);
+insert into option (option_id, option_value, poll_id) values (2, 'Blue', 1);
+insert into option (option_id, option_value, poll_id) values (3, 'Purple', 1);
+
+insert into poll (poll_id, question) values (2, 'Which drink do you prefer?');
+
+insert into option (option_id, option_value, poll_id) values (4, 'Coffee', 2);
+insert into option (option_id, option_value, poll_id) values (5, 'Tea', 2);
+insert into option (option_id, option_value, poll_id) values (6, 'Juice', 2);
+
+insert into poll (poll_id, question) values (3, 'Which fast food company do you prefer?');
+
+insert into option (option_id, option_value, poll_id) values (7, 'Burger King', 3);
+insert into option (option_id, option_value, poll_id) values (8, 'ChickFil-a', 3);
+insert into option (option_id, option_value, poll_id) values (9, 'Wendys', 3);
+
+insert into poll (poll_id, question) values (4, 'Which soda do you prefer?');
+
+insert into option (option_id, option_value, poll_id) values (10, 'Coke', 4);
+insert into option (option_id, option_value, poll_id) values (11, 'Dr.Pepper', 4);
+insert into option (option_id, option_value, poll_id) values (12, 'Sprite', 4);
+
+insert into poll (poll_id, question) values (5, 'Which phone do you prefer?');
+
+insert into option (option_id, option_value, poll_id) values (13, 'iPhone', 5);
+insert into option (option_id, option_value, poll_id) values (14, 'Samsung Galaxy', 5);
+insert into option (option_id, option_value, poll_id) values (15, 'Google Nexus', 5);
+
+insert into poll (poll_id, question) values (6, 'Which programming language do you prefer?');
+
+insert into option (option_id, option_value, poll_id) values (16, 'Java', 6);
+insert into option (option_id, option_value, poll_id) values (17, 'Python', 6);
+insert into option (option_id, option_value, poll_id) values (18, 'JavaScript', 6);
+
+insert into poll (poll_id, question) values (7, 'Which API framework do you prefer?');
+
+insert into option (option_id, option_value, poll_id) values (19, 'Red', 7);
+insert into option (option_id, option_value, poll_id) values (20, 'Blue', 7);
+insert into option (option_id, option_value, poll_id) values (21, 'Purple', 7);
+
+insert into poll (poll_id, question) values (8, 'Which databse software do you prefer?');
+
+insert into option (option_id, option_value, poll_id) values (22, 'MondoDB', 8);
+insert into option (option_id, option_value, poll_id) values (23, 'MySQL', 8);
+insert into option (option_id, option_value, poll_id) values (24, 'MariaDB', 8);
+
+insert into poll (poll_id, question) values (9, 'Which Integrated Develement Environment(IDE) do you prefer?');
+
+insert into option (option_id, option_value, poll_id) values (25, 'IntellJ', 9);
+insert into option (option_id, option_value, poll_id) values (26, 'Eclipse', 9);
+insert into option (option_id, option_value, poll_id) values (27, 'NetBeans', 9);
+
+insert into poll (poll_id, question) values (10, 'Which Cloud Computing service do you prefer?');
+
+insert into option (option_id, option_value, poll_id) values (28, 'Amazon Web Service', 10);
+insert into option (option_id, option_value, poll_id) values (29, 'Microsoft Azure', 10);
+insert into option (option_id, option_value, poll_id) values (30, 'Google Cloud', 10);
+
+insert into poll (poll_id, question) values (11, 'Which singer do you prefer?');
+
+insert into option (option_id, option_value, poll_id) values (31, 'Britney Spears', 11);
+insert into option (option_id, option_value, poll_id) values (32, 'Jennifer Lopez', 11);
+insert into option (option_id, option_value, poll_id) values (33, 'Miley Cyrus', 11);
+
+insert into poll (poll_id, question) values (12, 'Which actor do you prefer?');
+
+insert into option (option_id, option_value, poll_id) values (34, 'Denzel Washington', 12);
+insert into option (option_id, option_value, poll_id) values (35, 'Leonardo DiCaprio', 12);
+insert into option (option_id, option_value, poll_id) values (36, 'Brad Pitt', 12);
+
+insert into poll (poll_id, question) values (13, 'Which Disney movie do you prefer?');
+
+insert into option (option_id, option_value, poll_id) values (37, 'Aladdin', 13);
+insert into option (option_id, option_value, poll_id) values (38, 'Tangled', 13);
+insert into option (option_id, option_value, poll_id) values (39, 'Frozen', 13);
+
+insert into poll (poll_id, question) values (14, 'Which TV channel do you prefer?');
+
+insert into option (option_id, option_value, poll_id) values (40, 'ABC', 14);
+insert into option (option_id, option_value, poll_id) values (41, 'CBS', 14);
+insert into option (option_id, option_value, poll_id) values (42, 'NBC', 14);
+
+insert into poll (poll_id, question) values (15, 'Which operating system do you prefer?');
+
+insert into option (option_id, option_value, poll_id) values (43, 'Mac', 15);
+insert into option (option_id, option_value, poll_id) values (44, 'Windows', 15);
+insert into option (option_id, option_value, poll_id) values (45, 'Linux', 15);
+
