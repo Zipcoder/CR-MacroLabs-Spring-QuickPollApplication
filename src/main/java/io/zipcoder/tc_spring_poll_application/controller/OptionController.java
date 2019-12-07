414227d3828750dce0bfa932ccb79ff9b38a14bc
David Trombello <david@zipcoders-MacBook-Pro-96.local>
2019-12-07 18:24:19 -0500

    post mapping updated

--- src/main/java/io/zipcoder/tc_spring_poll_application/controller/OptionController.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controller/OptionController.java
@@ -1,4 +1,48 @@
 package io.zipcoder.tc_spring_poll_application.controller;
 
+
+import io.zipcoder.tc_spring_poll_application.domain.Poll;
+import io.zipcoder.tc_spring_poll_application.repositories.PollRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
 public class OptionController {
+
+    private PollRepository pollRepository;
+
+    @Autowired
+    public OptionController(PollRepository pollRepository){
+        this.pollRepository = pollRepository;
+    }
+
+    @GetMapping("/polls")
+    public ResponseEntity<Iterable<Poll>> getAllPolls() {
+        Iterable<Poll> allPolls = pollRepository.findAll();
+        return new ResponseEntity<>(allPolls, HttpStatus.OK);
+    }
+
+    @PostMapping("/polls")
+    public ResponseEntity<?> createPoll(@RequestBody Poll poll) {
+        pollRepository.save(poll);
+        URI newPollUri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(poll.getId())
+                .toUri();
+        HttpHeaders header = new HttpHeaders();
+        header.setLocation(newPollUri);
+        return new ResponseEntity<>(header, HttpStatus.CREATED);
+    }
+
+
+
+
 }
--- src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
@@ -1,4 +1,8 @@
 package io.zipcoder.tc_spring_poll_application.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PollController {
 }
