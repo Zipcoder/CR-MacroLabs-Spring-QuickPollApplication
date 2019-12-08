7188e096fc2645d494f5310efb5d557ff8e39548
David Trombello <david@zipcoders-MacBook-Pro-96.local>
2019-12-08 17:21:33 -0500

    dtos created

--- src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
@@ -1,8 +1,65 @@
 package io.zipcoder.tc_spring_poll_application.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import io.zipcoder.tc_spring_poll_application.domain.Poll;
+import io.zipcoder.tc_spring_poll_application.repositories.PollRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 public class PollController {
+
+    private PollRepository pollRepository;
+
+    @Autowired
+    public PollController(PollRepository pollRepository){
+        this.pollRepository = pollRepository;
+    }
+
+    @GetMapping("/polls")
+    public ResponseEntity<Iterable<Poll>> getAllPolls() {
+        Iterable<Poll> allPolls = pollRepository.findAll();
+        return new ResponseEntity<>(allPolls, HttpStatus.OK);
+    }
+
+    @PostMapping("/polls")
+    public ResponseEntity<?> createPoll(@RequestBody Poll poll) {
+        pollRepository.save(poll);
+        URI newPollUri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(poll.getId())
+                .toUri();
+        HttpHeaders header = new HttpHeaders();
+        header.setLocation(newPollUri);
+        return new ResponseEntity<>(header, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/polls/{pollId}")
+    public ResponseEntity<?> getPoll(@PathVariable Long pollId) {
+        if (pollRepository.findById(pollId).isPresent()) {
+            Poll p = pollRepository.findById(pollId).get();
+            return new ResponseEntity<>(p, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/polls/{pollId}")
+    public ResponseEntity<?> updatePoll(@RequestBody Poll poll, @PathVariable Long pollId) {
+        // Save the entity
+        Poll p = pollRepository.save(poll);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/polls/{pollId}")
+    public ResponseEntity<?> deletePoll(@PathVariable Long pollId) {
+        pollRepository.deleteById(pollId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
+
