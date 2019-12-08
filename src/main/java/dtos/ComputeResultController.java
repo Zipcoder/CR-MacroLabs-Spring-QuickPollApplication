6a0786ebe64b218eb9cb84420b0159bdb65e51d1
Charlotte Beale <charlottebeale@zipcoders-MacBook-Pro-6.local>
2019-12-08 15:27:15 -0500

    On part 5

---
+++ src/main/java/dtos/ComputeResultController.java
@@ -0,0 +1,31 @@
+package dtos;
+import io.zipcoder.tc_spring_poll_application.repositories.VoteRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.ResponseEntity;
+import io.zipcoder.tc_spring_poll_application.domain.Vote;
+import org.springframework.http.HttpStatus;
+
+
+public class ComputeResultController {
+    private VoteRepository voteRepository;
+
+
+    public ComputeResultController(VoteRepository voteRepository) {
+        this.voteRepository = voteRepository;
+    }
+
+    @RequestMapping(value = "/computeresult", method = RequestMethod.GET)
+    public ResponseEntity<?> computeResult(@RequestParam Long pollId) {
+        VoteResult voteResult = new VoteResult();
+        Iterable<Vote> allVotes = voteRepository.findVotesByPoll(pollId);
+
+        //TODO: Implement algorithm to count votes
+        return new ResponseEntity<VoteResult>(voteResult, HttpStatus.OK);
+    }
+
+
+
+
+}
--- src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
@@ -1,14 +1,18 @@
 package io.zipcoder.tc_spring_poll_application.controller;
+import io.zipcoder.tc_spring_poll_application.domain.*;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import io.zipcoder.tc_spring_poll_application.repositories.PollRepository;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import java.net.URI;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
 @RestController
 public class PollController {
    private PollRepository pollRepository;
+
 
    @Autowired
     public PollController(PollRepository pollRepository) {
@@ -18,6 +22,53 @@ public class PollController {
     @RequestMapping(value = "/polls", method = RequestMethod.GET)
     public ResponseEntity<Iterable<Poll>> getAllPolls() {
         Iterable<Poll> allPolls = pollRepository.findAll();
-        return new ResponseEntity<>(allPolls, org.springframework.http.HttpStatus.OK);
+        return new ResponseEntity<>(allPolls, HttpStatus.OK);
     }
+
+    @RequestMapping(value="/polls", method=RequestMethod.POST)
+    public ResponseEntity<?> createPoll(@RequestBody Poll poll) {
+        poll = pollRepository.save(poll);
+        URI newPollUri = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(poll.getId()).toUri();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(newPollUri);
+        return new ResponseEntity<>(null, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value="/polls/{pollId}", method=RequestMethod.GET)
+    public ResponseEntity<?> getPoll(@PathVariable Long pollId) {
+        Poll p = pollRepository.findOne(pollId);
+        return new ResponseEntity<> (p, HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/polls/{pollId}", method=RequestMethod.PUT)
+    public ResponseEntity<?> updatePoll(@RequestBody Poll poll, @PathVariable Long pollId) {
+        // Save the entity
+        Poll p = pollRepository.save(poll);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/polls/{pollId}", method=RequestMethod.DELETE)
+    public ResponseEntity<?> deletePoll(@PathVariable Long pollId) {
+        pollRepository.delete(pollId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    public void verifyPoll(){
+
+    }
+
+/* Create a void method in PollController called verifyPoll that checks if a specific
+ poll id exists and throws a ResourceNotFoundException if not. Use this in any method
+  that searches for or updates an existing poll (eg: Get, Put, and Delete methods).
+
+Note: This means that trying to submit a PUT request for a resource that doesn't exist
+will not implicitly create it; it should throw a 404 instead.
+
+*/
+
+
+
+
 }
