65dfc305bf8a2589c786ad95a3410b7add4bee37
vaidehi4321 <56602405+vaidehi4321@users.noreply.github.com>
2019-12-07 20:00:52 -0500

    finished third part

---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/domain/Poll.java
@@ -0,0 +1,42 @@
+package io.zipcoder.tc_spring_poll_application.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Poll {
+    @Id
+    @GeneratedValue
+    @Column(name = "POLL_ID")
+private Long id;
+    @Column(name = "QUESTION")
+    private String question;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "POLL_ID")
+    @OrderBy
+    Set<Option> options;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public Set<Option> getOptions() {
+        return options;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void setOptions(Set<Option> options) {
+        this.options = options;
+    }
+}
