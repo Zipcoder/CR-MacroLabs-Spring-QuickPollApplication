dede49efff4a79e62808703cf75a33bc56ffb0c1
SandyPham823 <sandydupham@gmail.com>
2019-12-11 23:26:03 -0500

    finishe lab

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into poll (poll_id, question) values (2, 'Which drink do you prefer?');
+
+insert into option (option_id, option_value, poll_id) values (4, 'Coffee', 2);
+insert into option (option_id, option_value, poll_id) values (5, 'Tea', 2);
+insert into option (option_id, option_value, poll_id) values (6, 'Juice', 2);
+
