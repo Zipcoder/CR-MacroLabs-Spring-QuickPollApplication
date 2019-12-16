07c3d4cf7c0b626248508354e0682f47db1a8fd7
John Tokhi <56596400+tokhij@users.noreply.github.com>
2019-12-15 21:27:32 -0500

    done i guess

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,6 @@
-insert into poll (poll_id, question) values (1, 'What is your favorite color?');
+insert into poll (poll_id, question) values (2, 'What is your favorite cars?');
 
-insert into option (option_id, option_value, poll_id) values (1, 'Red', 1);
+insert into option (option_id, option_value, poll_id) values (4, 'Mustang', 2);
+insert into option (option_id, option_value, poll_id) values (5, 'Corvette', 2);
+insert into option (option_id, option_value, poll_id) values (6, 'GTR', 2);
+insert into option (option_id, option_value, poll_id) values (7, 'M6', 2);
